4d596ce52451e26d502f9bad46c48290ca9562ee
Winston Fale <winstonfale@gmail.com>
2021-03-23 12:51:48 +0800

    add config

--- config/wallet.php
+++ config/wallet.php
@@ -5,7 +5,15 @@ return [
         'board_lot' => 6, //6 decimal round
         'buy_currency' => 'USDC', // wallet check for buying
         'sell_currency' => 'BTC', // wallet check for selling
-        'add_on_filler' => 0.000001, // filler for failed boardlot
-    ]
+        'add_on_filler' => 0.000001, // filler for failed boardlot on buying
+        'quantity_formula' => 'divider'
+    ],
 
+    'XRPBTC' => [
+        'board_lot' => 6, //6 decimal round
+        'buy_currency' => 'BTC', // wallet check for buying
+        'sell_currency' => 'XRP', // wallet check for selling
+        'add_on_filler' => 0, // filler for failed boardlot
+        'quantity_formula' => 'multiplier'
+    ],
 ];
