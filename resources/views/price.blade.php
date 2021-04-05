1c368c352b2459009873e8464514cbe8fb74bb07
Winston Fale <winstonfale@gmail.com>
2021-04-05 14:20:11 +0800

    add current price on price page

--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -5,7 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">BTCUSD History</div>
+                <div class="card-header">BTCUSD History
+                       <span class="float-right">
+                        <small>
+                            <strong>BTC:</strong>  {{ \Illuminate\Support\Facades\Cache::get('BTCUSDC') }} &nbsp; | &nbsp;
+                            <strong>RSI-1h-14: </strong>  {{ \Illuminate\Support\Facades\Cache::get('RSI_1D_14') }}  &nbsp;
+                        </small>
+                     </span>
+                </div>
                 <div class="card-body">
                     <table  class="table">
                         <thead class="thead-light">
